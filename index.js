3ab553fee889e9c264da85b6003f77661b7386e2
MARVIN <o.silverygold@gmail.com>
2023-06-05 13:25:03 +0300

    Solve linting errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { displayItem } from "../modules/displayItem.js";
-import { displayBooks } from "../modules/displayBooks.js";
+/* eslint-disable import/no-cycle */
+import displayItem from './modules/displayItem.js';
+import displayBooks from './modules/displayBooks.js';
 
 export const form = document.querySelector('form');
 export const bookTitle = document.querySelector('.title');
@@ -8,10 +9,10 @@ export const bookAuthor = document.querySelector('.author');
 displayItem();
 
 (function onReload() {
-    if(localStorage.getItem('books')){
-      const allBooks = JSON.parse(localStorage.getItem('books'));
-      displayBooks(allBooks);
-    }
+  if (localStorage.getItem('books')) {
+    const allBooks = JSON.parse(localStorage.getItem('books'));
+    displayBooks(allBooks);
+  }
 }());
 
 form.addEventListener('submit', (e) => {
@@ -25,26 +26,26 @@ const formSection = document.querySelector('.form-section');
 const contactSection = document.querySelector('.contact-section');
 
 pages.forEach((page) => {
-    page.addEventListener('click', (e) => {
-      if (e.target.classList.contains('list')) {
-        bookSection.classList.remove('non-active');
-        bookSection.classList.add('active');
-        formSection.classList.add('non-active');
-        contactSection.classList.add('non-active');
-      } else if (e.target.classList.contains('new')) {
-        bookSection.classList.remove('active');
-        bookSection.classList.add('non-active');
-        formSection.classList.remove('non-active');
-        formSection.classList.add('active');
-        contactSection.classList.remove('active');
-        contactSection.classList.add('non-active');
-      } else if (e.target.classList.contains('contact')) {
-        bookSection.classList.remove('active');
-        bookSection.classList.add('non-active');
-        formSection.classList.remove('active');
-        formSection.classList.add('non-active');
-        contactSection.classList.remove('non-active');
-        contactSection.classList.add('active');
-      }
-    });
+  page.addEventListener('click', (e) => {
+    if (e.target.classList.contains('list')) {
+      bookSection.classList.remove('non-active');
+      bookSection.classList.add('active');
+      formSection.classList.add('non-active');
+      contactSection.classList.add('non-active');
+    } else if (e.target.classList.contains('new')) {
+      bookSection.classList.remove('active');
+      bookSection.classList.add('non-active');
+      formSection.classList.remove('non-active');
+      formSection.classList.add('active');
+      contactSection.classList.remove('active');
+      contactSection.classList.add('non-active');
+    } else if (e.target.classList.contains('contact')) {
+      bookSection.classList.remove('active');
+      bookSection.classList.add('non-active');
+      formSection.classList.remove('active');
+      formSection.classList.add('non-active');
+      contactSection.classList.remove('non-active');
+      contactSection.classList.add('active');
+    }
   });
+});
